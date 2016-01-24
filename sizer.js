30ef3b4f926f07d3581d5eb5ec2823703795949a
shaykyasin <shayk.yasin@gmail.com>
2016-01-25 02:42:55 +0530

    Fixed sizer showing wrong size for non-text files

--- sizer.js
+++ sizer.js
@@ -1,18 +1,28 @@
+var fs = require('fs')
+
 module.exports = function(query, req, res) {
 	if(req.method == 'POST') {
-		var data = ''
-		req.on('data', function (chunk) {
-    		data += chunk
-  		})
+		var ip = req.headers["x-forwarded-for"] ? req.headers["x-forwarded-for"] : req.connection.remoteAddress
+		ip = ip.replace(/[\/:\\]/g, '')
+		var file = '/tmp/' + ip + 'at' + new Date().getTime()
+		var tempFile = fs.createWriteStream(file)
+
+		req.pipe(tempFile)
 
 		req.on('end', function(){
-			data = data.split('\r\n')
-			var newdata = data[4]
-			for(var i=5; i < data.length - 2; i++) {
-				newdata += '\r\n' + data[i]
-			}
-			res.writeHead(200, '{"Content-Type": "application/json"}')
-			res.end(JSON.stringify({fileSize: newdata.length}))
+			tempFile.on('finish', function(){
+				fs.readFile(file, function(err, data){
+					if(err) throw err
+					data = data.toString().split('\r\n')
+					data = data[0] + '\r\n' + data[1] + '\r\n' + data[2] + '\r\n' + data[3] + '\r\n' + data[data.length - 2] + '\r\n' + data[data.length - 1] + '\r\n' 
+					fs.stat(file, function(err, stat){
+						if(err) throw err
+						res.writeHead(200, '{"Content-Type": "application/json"}')
+						res.end(JSON.stringify({fileSize: stat.size - data.length}))
+						fs.unlink(file)
+					})
+				})
+			})
 		})
 	}
 }
